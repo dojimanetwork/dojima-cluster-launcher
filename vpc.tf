af53750d16dff9afd60996c8461324a9a65f1c6e
navaldojima <109265864+navaldojima@users.noreply.github.com>
2023-12-01 15:37:55 +0530

    Add files via upload

---
+++ vpc.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name           = var.cluster_name
+  cidr           = var.vpc_cidr
+  azs            = length(var.az) == 0 ? data.aws_availability_zones.available.names : [for az in var.az : format("%s%s", var.region, az)]
+  public_subnets = [cidrsubnet(var.vpc_cidr, 4, 1), cidrsubnet(var.vpc_cidr, 4, 2), cidrsubnet(var.vpc_cidr, 4, 3)]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+}
